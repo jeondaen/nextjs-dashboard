397982299e73933eeeaa492c4707b03a6c6dce89
jeondaen <jeondaen@gmail.com>
2024-08-13 15:01:28 +0900

    Streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,6 +16,7 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 4000))
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,6 +31,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
